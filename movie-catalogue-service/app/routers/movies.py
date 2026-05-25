a117ae1836b3dbf5b4954ca8bd37b42847942b67
caglayurdasal <caglayurdasal@gmail.com>
2026-05-25 22:53:19 +0200

    Add search endpoint

--- movie-catalogue-service/app/routers/movies.py
+++ movie-catalogue-service/app/routers/movies.py
@@ -24,3 +24,32 @@ def list_movies(page: int = 1, limit: int = Query(10, le=50)):
     # fetches movies from the collection
     movies = movie_collection.find().skip(offset).limit(limit)
     return [serialize(m) for m in movies]  # serializes the movies and returns them
+
+
+# search by query string
+@router.get("/search")
+def search(
+    q: str = Query(..., min_length=1), page: int = 1, limit: int = Query(10, le=50)
+):
+    """
+    Args:
+        q (int): Search quesry string.
+        page (int, optional): Page number to retrive. Defaults to 1.
+        limit (int, optional): Number of movies per page. Defaults 10, max 50.
+
+    Returns:
+        List: List of movies matching the search query and sorted by relevance.
+    """
+    offset = (page - 1) * limit
+
+    # filters with search string and relevance
+    results_unsorted = movie_collection.find(
+        {"$text": {"$search": q}}, {"score": {"$meta": "textScore"}}
+    )
+    # sorts results by relevance
+    results_sorted = (
+        results_unsorted.sort([("score", {"$meta": "textScore"})]))
+    
+    # displays results based on current page and limit
+    results_paginated= results_sorted.skip(offset).limit(limit)
+    return [serialize(m) for m in results_paginated]
